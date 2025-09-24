b09dae30a2ec49bd73d9cd509336a92e17bab1bb
dhirenpatel4u <102461109+dhirenpatel4u@users.noreply.github.com>
2025-09-24 13:04:34 +0530

    Update index.php

--- api/index.php
+++ api/index.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
   <title>Reel Player Debug</title>
-  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="/files/style.css" />
   <meta http-equiv="Content-Security-Policy"
         content="default-src 'self'; script-src 'self' https://script.google.com; style-src 'self'; img-src 'self' data:;">
 </head>
